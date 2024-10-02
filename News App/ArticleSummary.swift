7413bce7ba24a8e7ed152c2b03a5a3372bf2c527
youssef <youssefhsn16@gmail.com>
2024-10-02 13:47:25 +0300

    🛠️ Add media attribute to JSON structure

--- News App/ArticleSummary.swift
+++ News App/ArticleSummary.swift
@@ -13,7 +13,15 @@ let dummyArticle = Article(
     description: "This is a brief description of the article about SwiftUI's growing influence in the development world.",
     author: "John Doe",
     publishedAt: "2024-10-02",
-    url: "https://www.example.com/swiftui-takes-over"
+    url: "https://www.example.com/swiftui-takes-over",
+    media: Media(
+               mediaMetadata: [
+                   MediaMetadata(
+                       url: "https://api.dicebear.com/9.x/icons/svg", // Sample Dicebear URL
+                       format: "Standard Thumbnail"
+                   )
+               ]
+            )
 )
 
 struct Article: Codable, Identifiable {
@@ -23,13 +31,29 @@ struct Article: Codable, Identifiable {
     let author: String
     let publishedAt: String
     let url: String
+    let media: Media
     
     enum codingKeys: String, CodingKey {
-        case id, title, url
+        case id, title, url, media
         case description = "abstract"
         case author = "byline"
         case publishedAt = "published_date"
     }
+}
+
+struct Media: Codable {
+    let mediaMetadata: [MediaMetadata]
+    
+    enum CodingKeys: String, CodingKey {
+        case mediaMetadata = "media-metadata"
+    }
+}
+
+struct MediaMetadata: Codable {
+    let url: String
+    let format: String
+//    let height: Int
+//    let width: Int
 }
 
 struct ArticleSummary: View {
---
+++ News App/NewsService.swift
@@ -0,0 +1,8 @@
+//
+//  NewsService.swift
+//  News App
+//
+//  Created by Youssef Al-Hassan on 02/10/2024.
+//
+
+import Foundation
